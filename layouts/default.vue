9dc31215077a9b50a00872a53877ef3408358d73
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-06-30 03:17:06 +0300

    Default layout has been designed

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRepositoriesStore } from "~/store/useRepositories";
+import { useGistsStore } from "~/store/useGists";
+
+const { init: initRepositories } = useRepositoriesStore();
+const { init: initGists } = useGistsStore();
+
+initRepositories();
+initGists()
+</script>
+<template>
+  <div class="min-h-screen flex flex-col bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500">
+    <Header/>
+    <main
+        class="grid container flex-grow mx-auto px-5 py-8 backdrop-filter backdrop-blur-lg bg-rgba(255, 255, 255, 0.1)">
+      <slot/>
+    </main>
+    <Footer/>
+  </div>
+</template>
+
